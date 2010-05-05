3ebdb4be17eea248b972bc56e980a03257b96cb4
Dom Derrien <dominique.derrien@gmail.com>
2010-05-05 11:49:36 -0400

    minor edit

--- build/manual-deploy.bat
+++ build/manual-deploy.bat
@@ -1,4 +1,2 @@
-call deploy.bat
-
 REM Assume ${temp.dir} is set to %TEMP%\webapp, in ${user.dir}\local.build.properties
-bin\appengine-java-sdk-1.3.2\bin\appcfg.cmd --email=dominique.derrien@gmail.com update %TEMP%\webapp\appengine-stage
+bin\appengine-java-sdk-1.3.3\bin\appcfg.cmd --email=dominique.derrien@gmail.com update %TEMP%\webapp\appengine-stage
