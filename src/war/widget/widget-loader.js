a7fc6678ef6d743d8380f687d2d12575b9ef70d2
Dom Derrien <dominique.derrien@gmail.com>
2010-11-17 15:14:31 -0500

    Preparation of the widget & tab loaders to be embedded in 3rd party websites
    Signed-off-by: Dom Derrien <dominique.derrien@gmail.com>

--- src/war/widget/widget-loader.js
+++ src/war/widget/widget-loader.js
@@ -2,19 +2,30 @@
  * @author Dom Derrien
  */
 (function() {
-    var lg = location.search.indexOf('lg=fr') != -1 ? 'fr' : 'en',
-        dm = 'https://anothersocialeconomy.appspot.com/widget/',
-        id = "__ASE_wdgt",
+    var aC = ase_config || { lg: 'en', referralId: '0' },
+        lg = aC.lg,
+        ht = aC.hashtag || 'core',
+        dm = '../../widget/', // 'https://anothersocialeconomy.appspot.com/widget/',
+        id = 'ase_widget',
         dc = document,
+        bd = {
+            core: { fr: 'Réseau d\'achat locaux', en: 'Local Buying Network' },
+            cardealer: { fr: 'Rechercher un véhicule d\'occasion', en: 'Find Local Pre-Owned Cars' }
+        }
         installWidget = function(){
             var dv = dc.getElementById(id),
-                bd = lg == 'fr' ? 'Réseau d\'achat locaux?' : 'Local Buying Network?',
-                cl = lg == 'fr' ? 'Fermer [X]' : 'Close [X]',
                 txt = escape(window.getSelection ? window.getSelection() : dc.getSelection ? dc.getSelection() : dc.selection ? dc.selection.createRange().text : ''),
-                src = dm + 'ase.jsp?lg=' + lg + '&brand=' + bd + '&countryCode=CA&criteria=' + txt + '&',
-                ifr;
+                src = dm + (aC.hashtag || 'ase') + '.jsp?criteria=' + txt,
+                ifr, p;
+            for(p in aC) {
+                src += '&' + p + '=' + escape(aC[p]);
+            }
+            if (!aC.brand) {
+                src += '&brand=' + escape(bd[ht][lg]);
+            }
+            console.log(src.replace(/\&/g, '\n'));
             if (dv) {
-                ifr = dc.getElementById(id + '_ifr');
+                ifr = dc.getElementById(id + 'Ifr');
                 if (txt == '' || ifr.src.indexOf('criteria=' + txt + '&') != -1) {
                     dv.style.display = '';
                 }
@@ -24,20 +35,22 @@
                 }
             }
             if (!dv) {
-                var cpc = '100%',
+                var cl = lg == 'fr' ? 'Fermer [X]' : 'Close [X]',
+                    cpc = '100%',
                     hd;
                 dv = dc.createElement('div');
                 dv.id = id;
-                dv.setAttribute('class', 'ase_buyItWidget');
+                dv.setAttribute('class', 'ase_widget');
                 dc.getElementsByTagName('body')[0].appendChild(dv);
                 hd = dc.createElement('span');
                 hd.innerHTML = '<a href="#" onclick="document.getElementById(\''+id+'\').style.display=\'none\';return false;">'+ cl + '<a>';
                 dv.appendChild (hd);
                 ifr = dc.createElement('iframe');
-                ifr.id = id + '_ifr';
+                ifr.id = id + 'Ifr';
                 ifr.width = cpc;
                 ifr.height = cpc;
                 ifr.border = '0';
+                ifr.frameBorder = '0';
                 ifr.src = src;
                 dv.appendChild(ifr);
             }
@@ -50,20 +63,31 @@
             lk.type = 'text/css';
             dc.getElementsByTagName('head')[0].appendChild(lk);
         },
-        installTab = function(){
-            var hk = lg == 'fr' ? 'Achetez localement?' : 'Buy it locally?',
-                ac = dc.createElement('a'),
+        installTab = function() {
+            var ac = dc.createElement('a'),
                 dv = dc.createElement('div');
             ac.href = '#';
+            ac.id = 'ase_floatingTab';
             ac.onclick = installWidget;
-            ac.appendChild(dc.createTextNode(hk));
-            dv.setAttribute("class", 'ase_buyItTab');
+            ac.appendChild(dc.createTextNode(aC.brand || bd[ht][lg]));
+            if (aC['color-brand']) {
+                ac.style.color = aC['color-brand'];
+            }
+            dv.setAttribute('class', 'ase_floatingTab');
             dv.appendChild(ac);
+            if (aC['border']) {
+                dv.style.border = aC['border'];
+            }
+            if (aC['background-color']) {
+                dv.style.backgroundColor = aC['background-color'];
+            }
             dc.getElementsByTagName('body')[0].appendChild(dv);
-            if (document.all) { dv.style.top = '100px'; }
+            if (document.all) {
+                dv.style.top = '100px';
+            }
         };
     installCSS();
-    if (typeof window.ase_showWidget == "undefined") {
+    if (typeof window.ase_showWidget == 'undefined') {
         installTab();
     }
     else {
